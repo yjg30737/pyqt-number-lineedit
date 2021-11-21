e3388287896acf3e4b61d85956d21e5fff0db5c1
Jung Gyu Yoon <yjg30737@gmail.com>
2021-11-21 19:41:33 +0900

    update

--- number_lineedit/numberLineEdit.py
+++ number_lineedit/numberLineEdit.py
@@ -6,13 +6,22 @@ from PyQt5.QtWidgets import QLineEdit, QSizePolicy
 class NumberLineEdit(QLineEdit):
     def __init__(self):
         super().__init__()
+        self.__comma_flag = True
         self.__initUi()
 
     def __initUi(self):
         self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         self.setValidator(QRegExpValidator(QRegExp('^([1-9][0-9]+\.?|0\.)[0-9]+$'), self))
 
+    def textChanged(self, a0: str) -> None:
+        if self.__comma_flag:
+            self.__setCommaToLineEditNumber(self.__comma_flag)
+        return super().textChanged(a0)
+
     def setComma(self, f: bool):
+        self.__comma_flag = f
+
+    def __setCommaToLineEditNumber(self, f: bool):
         text = self.text()
         if text:
             if f:
