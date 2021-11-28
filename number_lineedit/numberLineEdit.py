a1ed0dcf52bfca326316e39da37c6f5430ea1ded
Jung Gyu Yoon <yjg30737@gmail.com>
2021-11-28 13:52:48 +0900

    Apply textChanged event

--- number_lineedit/numberLineEdit.py
+++ number_lineedit/numberLineEdit.py
@@ -6,16 +6,25 @@ from PyQt5.QtWidgets import QLineEdit, QSizePolicy
 class NumberLineEdit(QLineEdit):
     def __init__(self):
         super().__init__()
+        self.__comma_enabled = False
         self.__initUi()
 
     def __initUi(self):
         self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         self.setValidator(QRegExpValidator(QRegExp('^([1-9][0-9]+\.?|0\.)[0-9]+$'), self))
 
+    def textChanged(self, text):
+        self.setCommaToText()
+        return super().textChanged(text)
+
     def setComma(self, f: bool):
+        self.__comma_enabled = f
+        self.setCommaToText()
+
+    def setCommaToText(self):
         text = self.text()
         if text:
-            if f:
+            if self.__comma_enabled:
                 if text.find('.') == -1:
                     self.setText('{:,}'.format(int(text)))
                 else:
