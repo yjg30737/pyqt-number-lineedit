00bf465960efb9c8bf5368af4cef8d9228a56929
Jung Gyu Yoon <yjg30737@gmail.com>
2021-11-28 14:01:05 +0900

    Improve textChanged event

--- number_lineedit/numberLineEdit.py
+++ number_lineedit/numberLineEdit.py
@@ -12,12 +12,12 @@ class NumberLineEdit(QLineEdit):
     def __initUi(self):
         self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
         self.setValidator(QRegExpValidator(QRegExp('^([1-9][0-9]+\.?|0\.)[0-9]+$'), self))
+        self.textChanged.connect(self.__textChanged)
 
-    def textChanged(self, text):
+    def __textChanged(self, text):
         print('textChanged called: {0}'.format(text))
         if self.__comma_enabled:
             self.setCommaToText()
-        return super().textChanged(text)
 
     def setComma(self, f: bool):
         self.__comma_enabled = f
