6e018d25b609b83c993882428d6e45c5c9fc7b59
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 07:19:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-number-lineedit',
@@ -9,6 +16,8 @@ setup(
     packages=find_packages(),
     description='QLineEdit which allows only numeric input',
     url='https://github.com/yjg30737/pyqt-number-lineedit.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
